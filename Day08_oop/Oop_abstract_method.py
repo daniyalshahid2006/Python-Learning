b5501954ca9c671220529cc3dc46bef51fc790b8
Daniyal Shahid <daniyalshahid2006@gmail.com>
2026-08-10 20:43:07 +0500

    tests for abstarct

--- Day08_oop/Oop_abstract_method.py
+++ Day08_oop/Oop_abstract_method.py
@@ -89,3 +89,136 @@
 # Mage1.attack(Mage1,2,3,4)
 # Mage1.display()
 #
+import random
+from abc import ABC, abstractmethod
+class Character(ABC):
+    def __init__(self, name,health):
+        self.name = name
+        self.__health = health
+    def display(self):
+        print(self.name)
+        print(self.__health)
+    def get_health(self):
+        return self.__health
+    def set_health(self,new_health):
+        if new_health > 100:
+            self.__health = 100
+        elif new_health < 0:
+            self.__health = 0
+        else:
+            self.__health = new_health
+    def damage(self,amount):
+        new_health = self.__health - amount
+        self.set_health(new_health)
+        print(f"{self.name} takes {amount} of damage")
+    @abstractmethod
+    def attack(self,attacks):
+        pass
+class Warrior(Character):
+    def __init__(self,name,health,stamina):
+        super().__init__(name,health)
+        self.stamina = stamina
+    def display(self):
+        print(self.name)
+        print(self.get_health())
+        print(self.stamina)
+    def attack(self,attacks):
+      if attacks != self:
+        if self.stamina >= 5:
+            print(f"{self.name} do a basic attacks {attacks} with a sword ")
+            damage = 20
+            attacks.damage(damage)
+            self.stamina = self.stamina- 5
+        else:
+            print(f"{self.name} have insufficient stamina")
+      else:
+          print(f"cant attack thyself")
+    def heavy_attack(self,attacks):
+      if attacks != self:
+        if self.stamina >= 20:
+            print(f"{self.name} do a heavy attacks {attacks} with a sword ")
+            damage = 40
+            attacks.damage(damage)
+            self.stamina = self.stamina- 20
+        else:
+            print(f"{self.name} have insufficient stamina")
+      else:
+          print(f"cant attack thyself")
+class Mage(Character):
+    def __init__(self,name,health,mana):
+        super().__init__(name,health)
+        self.mana = mana
+    def display(self):
+        print(self.name)
+        print(self.get_health())
+        print(self.mana)
+    def attack(self,attacks):
+      if attacks != self:
+        if self.mana >= 10:
+            print(f"{self.name} cast a magic attack {attacks}")
+            damage = 25
+            attacks.damage(damage)
+            self.mana = self.mana - 5
+        else:
+            print(f"{self.name} have insufficient mana")
+      else:
+            print(f"cant attack thyself")
+    def fire_ball(self,attacks,burnt):
+      if attacks != self:
+        if self.mana >= 30:
+            print(f"{self.name} cast a fire ball")
+            damage = 50
+            if random.random() < 0.4:
+                damage =  damage + 10
+                print(f"{self.name} cast burnt")
+            attacks.damage(damage)
+            self.mana = self.mana - 30
+        else:
+            print(f"{self.name} have insufficient mana")
+      else:
+          print(f"cant attack thyself")
+
+class Goblin(Character):
+    def __init__(self,name,health,poison):
+        super().__init__(name,health)
+        self.poison = poison
+    def display(self):
+        print(self.name)
+        print(self.get_health())
+        print(self.poison)
+    def attack(self,attacks):
+        if attacks != self:
+            if self.poison >= 5:
+                print(f"{self.name} attack with poison {attacks}")
+                damage = 20
+                if random.random() < 0.4:
+                    damage = damage + 2+2+2+2+2
+                    print(f"{self.name} cast poison")
+                attacks.damage(damage)
+                self.poison = self.poison - 5
+            else:
+                print(f"{self.name} have insufficient poison")
+        else:
+            print(f"cant attack thyself")
+
+name1 = input("What is your name?")
+health1 = int(input("What is your health?"))
+stamina1 = int(input("What is your stamina?"))
+Warrior1 = Warrior(name1,health1,stamina1)
+Warrior1.display()
+print(Warrior1.get_health())
+Warrior1.attack(Warrior1)
+print(Warrior1.get_health())
+goblin1 = Goblin(name1,health1,stamina1)
+goblin1.display()
+print(goblin1.get_health())
+Warrior1.attack(goblin1)
+print(goblin1.get_health())
+mage1 = Mage(name1,health1,stamina1)
+mage1.display()
+print(mage1.get_health())
+mage1.attack(mage1)
+print(mage1.get_health())
+
+
+
