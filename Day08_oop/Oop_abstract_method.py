9f58f51ceeb61f15d8a096315805cc8beeb3e458
Daniyal Shahid <daniyalshahid2006@gmail.com>
2026-08-10 23:16:54 +0500

    hard test

--- Day08_oop/Oop_abstract_method.py
+++ Day08_oop/Oop_abstract_method.py
@@ -1,10 +1,103 @@
+# # from abc import ABC, abstractmethod
+# # class Character(ABC):
+# #     def __init__(self,name,health):
+# #         self.name = name
+# #         self.__health = health
+# #     def display(self):
+# #         print(f"{self.name} have {self.__health} health")
+# #     def get_health(self):
+# #         return self.__health
+# #     def set_health(self,new_health):
+# #         if new_health > 100:
+# #             self.__health = 100
+# #         elif new_health < 0:
+# #             self.__health = 0
+# #         else:
+# #             self.__health = new_health
+# #     def damage(self,amount):
+# #         new_health = self.__health - amount
+# #         self.set_health(new_health)
+# #         print(f"{self.name} takes {amount} amount of damage")
+# #     @abstractmethod
+# #     def attack(self,attacks):
+# #         pass
+# # class Warrior(Character):
+# #     def __init__(self,name,health,stamina):
+# #         super().__init__(name,health)
+# #         self.stamina = stamina
+# #     def display(self):
+# #         print(f"{self.name} have {self.get_health()} health and {self.stamina} stamina")
+# #     def attack(self,attacks,*bleeding):
+# #         if self.stamina >= 5:
+# #             print(f"{self.name} attacks with a sword")
+# #             damage = 20
+# #             if bleeding:
+# #                 damage = damage + sum(bleeding)
+# #                 print(f"{self.name} caused bleeding ")
+# #             attacks.damage(damage)
+# #             self.stamina = self.stamina - 5
+# #         else:
+# #             print(f"{self.name} have insufficient stamina")
+# # class Mage(Character):
+# #     def __init__(self,name,health,mana):
+# #         super().__init__(name,health)
+# #         self.mana = mana
+# #     def display(self):
+# #         print(f"{self.name} have {self.get_health()} health and {self.mana} mana")
+# #     def attack(self,attacks,*burn):
+# #         if self.mana >= 10:
+# #             print(f"{self.name} cast a fireball")
+# #             damage = 25
+# #             if burn:
+# #                 damage = damage + sum(burn)
+# #                 print(f"{self.name} caused burn")
+# #             attacks.damage(damage)
+# #             self.mana = self.mana - 10
+# #         else:
+# #             print(f"{self.name} have insufficient mana")
+# # class Goblin(Character):
+# #     def __init__(self,name,health,**kwargs):
+# #         super().__init__(name,health)
+# #         self.info = kwargs
+# #     def display(self):
+# #         print(f"{self.name} have {self.get_health()} health and {self.info}")
+# #     def attack(self,attacks):
+# #         pass
+# # name1 = input("What is your name?")
+# # health1 = int(input("What is your health?"))
+# # strength1 = int(input("What is your strength?"))
+# # Warrior1 = Warrior(name1,health1,strength1)
+# # Warrior1.display()
+# # print(Warrior1.get_health())
+# # name2 = input("What is your name?")
+# # health2 = int(input("What is your health?"))
+# # goblin1 = Goblin(name2,health2,poison = 23)
+# # goblin1.display()
+# # print(goblin1.get_health())
+# # Warrior1.attack(goblin1,2,3,4)
+# # Warrior1.display()
+# # print(goblin1.get_health())
+# # name3 = input("What is your name?")
+# # health3 = int(input("What is your health?"))
+# # mana1 = int(input("What is your mana?"))
+# # Mage1 = Mage(name3,health3,mana1)
+# # Mage1.display()
+# # print(Mage1.get_health())
+# # Mage1.attack(goblin1,2,3,4)
+# # Mage1.display()
+# # print(goblin1.get_health())
+# # Mage1.attack(Mage1,2,3,4)
+# # Mage1.display()
+# #
+# import random
 # from abc import ABC, abstractmethod
 # class Character(ABC):
-#     def __init__(self,name,health):
+#     def __init__(self, name,health):
 #         self.name = name
 #         self.__health = health
 #     def display(self):
-#         print(f"{self.name} have {self.__health} health")
+#         print(self.name)
+#         print(self.__health)
 #     def get_health(self):
 #         return self.__health
 #     def set_health(self,new_health):
@@ -17,7 +110,7 @@
 #     def damage(self,amount):
 #         new_health = self.__health - amount
 #         self.set_health(new_health)
-#         print(f"{self.name} takes {amount} amount of damage")
+#         print(f"{self.name} takes {amount} of damage")
 #     @abstractmethod
 #     def attack(self,attacks):
 #         pass
@@ -26,199 +119,202 @@
 #         super().__init__(name,health)
 #         self.stamina = stamina
 #     def display(self):
-#         print(f"{self.name} have {self.get_health()} health and {self.stamina} stamina")
-#     def attack(self,attacks,*bleeding):
+#         print(self.name)
+#         print(self.get_health())
+#         print(self.stamina)
+#     def attack(self,attacks):
+#       if attacks != self:
 #         if self.stamina >= 5:
-#             print(f"{self.name} attacks with a sword")
+#             print(f"{self.name} do a basic attacks {attacks} with a sword ")
 #             damage = 20
-#             if bleeding:
-#                 damage = damage + sum(bleeding)
-#                 print(f"{self.name} caused bleeding ")
 #             attacks.damage(damage)
-#             self.stamina = self.stamina - 5
+#             self.stamina = self.stamina- 5
 #         else:
 #             print(f"{self.name} have insufficient stamina")
+#       else:
+#           print(f"cant attack thyself")
+#     def heavy_attack(self,attacks):
+#       if attacks != self:
+#         if self.stamina >= 20:
+#             print(f"{self.name} do a heavy attacks {attacks} with a sword ")
+#             damage = 40
+#             attacks.damage(damage)
+#             self.stamina = self.stamina- 40
+#         else:
+#             print(f"{self.name} have insufficient stamina")
+#       else:
+#           print(f"cant attack thyself")
 # class Mage(Character):
 #     def __init__(self,name,health,mana):
 #         super().__init__(name,health)
 #         self.mana = mana
 #     def display(self):
-#         print(f"{self.name} have {self.get_health()} health and {self.mana} mana")
-#     def attack(self,attacks,*burn):
+#         print(self.name)
+#         print(self.get_health())
+#         print(self.mana)
+#     def attack(self,attacks):
+#       if attacks != self:
 #         if self.mana >= 10:
-#             print(f"{self.name} cast a fireball")
+#             print(f"{self.name} cast a magic attack {attacks}")
 #             damage = 25
-#             if burn:
-#                 damage = damage + sum(burn)
-#                 print(f"{self.name} caused burn")
 #             attacks.damage(damage)
 #             self.mana = self.mana - 10
 #         else:
 #             print(f"{self.name} have insufficient mana")
+#       else:
+#             print(f"cant attack thyself")
+#     def fire_ball(self,attacks,burnt):
+#       if attacks != self:
+#         if self.mana >= 30:
+#             print(f"{self.name} cast a fire ball")
+#             damage = 50
+#             if random.random() < 0.4:
+#                 damage =  damage + 10
+#                 print(f"{self.name} cast burnt")
+#             attacks.damage(damage)
+#             self.mana = self.mana - 30
+#         else:
+#             print(f"{self.name} have insufficient mana")
+#       else:
+#           print(f"cant attack thyself")
+#
 # class Goblin(Character):
-#     def __init__(self,name,health,**kwargs):
+#     def __init__(self,name,health,poison):
 #         super().__init__(name,health)
-#         self.info = kwargs
+#         self.poison = poison
 #     def display(self):
-#         print(f"{self.name} have {self.get_health()} health and {self.info}")
+#         print(self.name)
+#         print(self.get_health())
+#         print(self.poison)
 #     def attack(self,attacks):
-#         pass
+#         if attacks != self:
+#             if self.poison >= 5:
+#                 print(f"{self.name} attack with poison {attacks}")
+#                 damage = 20
+#                 if random.random() < 0.4:
+#                     damage = damage + 2+2+2+2+2
+#                     print(f"{self.name} cast poison")
+#                 attacks.damage(damage)
+#                 self.poison = self.poison - 5
+#             else:
+#                 print(f"{self.name} have insufficient poison")
+#         else:
+#             print(f"cant attack thyself")
+#
 # name1 = input("What is your name?")
 # health1 = int(input("What is your health?"))
-# strength1 = int(input("What is your strength?"))
-# Warrior1 = Warrior(name1,health1,strength1)
+# stamina1 = int(input("What is your stamina?"))
+# Warrior1 = Warrior(name1,health1,stamina1)
 # Warrior1.display()
 # print(Warrior1.get_health())
-# name2 = input("What is your name?")
-# health2 = int(input("What is your health?"))
-# goblin1 = Goblin(name2,health2,poison = 23)
+# Warrior1.attack(Warrior1)
+# print(Warrior1.get_health())
+# goblin1 = Goblin(name1,health1,stamina1)
 # goblin1.display()
 # print(goblin1.get_health())
-# Warrior1.attack(goblin1,2,3,4)
-# Warrior1.display()
+# Warrior1.attack(goblin1)
 # print(goblin1.get_health())
-# name3 = input("What is your name?")
-# health3 = int(input("What is your health?"))
-# mana1 = int(input("What is your mana?"))
-# Mage1 = Mage(name3,health3,mana1)
-# Mage1.display()
-# print(Mage1.get_health())
-# Mage1.attack(goblin1,2,3,4)
-# Mage1.display()
-# print(goblin1.get_health())
-# Mage1.attack(Mage1,2,3,4)
-# Mage1.display()
+# mage1 = Mage(name1,health1,stamina1)
+# mage1.display()
+# print(mage1.get_health())
+# mage1.attack(mage1)
+# print(mage1.get_health())
 #
-import random
+#
+#
+
+
 from abc import ABC, abstractmethod
-class Character(ABC):
-    def __init__(self, name,health):
+class BankAccount(ABC):
+    def __init__(self,name,account_number, balance):
         self.name = name
-        self.__health = health
+        self.account_number = account_number
+        self.__balance = balance
+    def get_balance(self):
+        return self.__balance
+    def set_balance(self,amount):
+        if amount > 0:
+            self.__balance += amount
     def display(self):
-        print(self.name)
-        print(self.__health)
-    def get_health(self):
-        return self.__health
-    def set_health(self,new_health):
-        if new_health > 100:
-            self.__health = 100
-        elif new_health < 0:
-            self.__health = 0
+        print(f"{self.name}'s whose account number is {self.account_number} balance is {self.__balance}")
+    def deposit(self,amount):
+        if amount > 0:
+           self.__balance += amount
         else:
-            self.__health = new_health
-    def damage(self,amount):
-        new_health = self.__health - amount
-        self.set_health(new_health)
-        print(f"{self.name} takes {amount} of damage")
+            print(f"please enter a right amount")
+
     @abstractmethod
-    def attack(self,attacks):
+    def withdraw(self,amount):
         pass
-class Warrior(Character):
-    def __init__(self,name,health,stamina):
-        super().__init__(name,health)
-        self.stamina = stamina
+class Savings(BankAccount):
+    def __init__(self,name,account_number,balance):
+        super().__init__(name,account_number,balance)
     def display(self):
-        print(self.name)
-        print(self.get_health())
-        print(self.stamina)
-    def attack(self,attacks):
-      if attacks != self:
-        if self.stamina >= 5:
-            print(f"{self.name} do a basic attacks {attacks} with a sword ")
-            damage = 20
-            attacks.damage(damage)
-            self.stamina = self.stamina- 5
-        else:
-            print(f"{self.name} have insufficient stamina")
-      else:
-          print(f"cant attack thyself")
-    def heavy_attack(self,attacks):
-      if attacks != self:
-        if self.stamina >= 20:
-            print(f"{self.name} do a heavy attacks {attacks} with a sword ")
-            damage = 40
-            attacks.damage(damage)
-            self.stamina = self.stamina- 20
-        else:
-            print(f"{self.name} have insufficient stamina")
-      else:
-          print(f"cant attack thyself")
-class Mage(Character):
-    def __init__(self,name,health,mana):
-        super().__init__(name,health)
-        self.mana = mana
-    def display(self):
-        print(self.name)
-        print(self.get_health())
-        print(self.mana)
-    def attack(self,attacks):
-      if attacks != self:
-        if self.mana >= 10:
-            print(f"{self.name} cast a magic attack {attacks}")
-            damage = 25
-            attacks.damage(damage)
-            self.mana = self.mana - 5
-        else:
-            print(f"{self.name} have insufficient mana")
-      else:
-            print(f"cant attack thyself")
-    def fire_ball(self,attacks,burnt):
-      if attacks != self:
-        if self.mana >= 30:
-            print(f"{self.name} cast a fire ball")
-            damage = 50
-            if random.random() < 0.4:
-                damage =  damage + 10
-                print(f"{self.name} cast burnt")
-            attacks.damage(damage)
-            self.mana = self.mana - 30
-        else:
-            print(f"{self.name} have insufficient mana")
-      else:
-          print(f"cant attack thyself")
-
-class Goblin(Character):
-    def __init__(self,name,health,poison):
-        super().__init__(name,health)
-        self.poison = poison
-    def display(self):
-        print(self.name)
-        print(self.get_health())
-        print(self.poison)
-    def attack(self,attacks):
-        if attacks != self:
-            if self.poison >= 5:
-                print(f"{self.name} attack with poison {attacks}")
-                damage = 20
-                if random.random() < 0.4:
-                    damage = damage + 2+2+2+2+2
-                    print(f"{self.name} cast poison")
-                attacks.damage(damage)
-                self.poison = self.poison - 5
+        print(f"{self.name}'s whose account number is {self.account_number} savings are {self.get_balance()}")
+    def withdraw(self,amount):
+        if amount > 0:
+            if self.get_balance() >= amount:
+                self.__balance -= amount
+                print(f"{amount} is withdrawn")
             else:
-                print(f"{self.name} have insufficient poison")
+                print(f" you dont have enough money to withdraw {amount}")
         else:
-            print(f"cant attack thyself")
+            print(f"please enter a right amount")
+    def interset(self,amount,time):
+        for year in range(1,time+1):
+            tier = amount // 100000
+            rate = tier * 5
+            print(f"year {year} : Rs {amount:.2f} with rate {rate}%")
+            amount += amount*(rate/100)
+class CurrentAccount(BankAccount):
+    def __init__(self,name,account_number,balance,limit):
+        super().__init__(name, account_number, balance)
+        self.limit = limit
+    def display(self):
+        print(f"{self.name}'s whose account number is {self.account_number} savings are {self.get_balance()}")
+    def withdraw(self,amount):
+        if amount > 0:
+            if self.__balance >= amount:
+                self.__balance -= amount
+                print(f"{amount} is withdrawn")
+                extra = input("do you want to withdraw more money? y/n").lower()
+                if extra == "y":
+                    amount2 = int(input("how much money to withdraw?"))
+                    if amount2 <= self.limit:
+                        self.limit -= amount2
+                    else:
+                        print(f"insufficient money you can withdraw only {self.limit}")
+                else:
+                    pass
+            else:
+                print(f"insufficient money")
+        else:
+            print(f"please enter a right amount")
+name1 = input("Enter your first name: ")
+account_number1 = int(input("Enter your account number: "))
+balance1 = int(input("Enter your first balance: "))
+savings1 = Savings(name1,account_number1,balance1)
+savings1.display()
+print(savings1.get_balance())
+savings1.deposit(1000000)
+savings1.display()
+savings1.deposit(0)
+savings1.deposit(-1222)
+savings1.withdraw(100000)
+savings1.interset(100000,10)
+name2 = input("Enter your second name: ")
+account_number2 = int(input("Enter your account number: "))
+balance2 = int(input("Enter your second balance: "))
+current1 = CurrentAccount(name2,account_number2,balance2)
+current1.display()
+print(current1.get_balance())
+current1.deposit(100000)
+current1.display()
+current1.deposit(-1222)
+current1.display()
+current1.withdraw(100000)
+current1.display()
 
-name1 = input("What is your name?")
-health1 = int(input("What is your health?"))
-stamina1 = int(input("What is your stamina?"))
-Warrior1 = Warrior(name1,health1,stamina1)
-Warrior1.display()
-print(Warrior1.get_health())
-Warrior1.attack(Warrior1)
-print(Warrior1.get_health())
-goblin1 = Goblin(name1,health1,stamina1)
-goblin1.display()
-print(goblin1.get_health())
-Warrior1.attack(goblin1)
-print(goblin1.get_health())
-mage1 = Mage(name1,health1,stamina1)
-mage1.display()
-print(mage1.get_health())
-mage1.attack(mage1)
-print(mage1.get_health())
 
 
 
