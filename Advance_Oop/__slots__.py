94fee30bb48e8684a8c85965b5d5ac2dea66acc0
Daniyal Shahid <daniyalshahid2006@gmail.com>
2026-08-24 16:55:59 +0500

    __slots__ done

---
+++ Advance_Oop/__slots__.py
@@ -0,0 +1,24 @@
+# class Player:
+#     __slots__ = ["name","health","level"]
+#     def __init__(self,name,health,level):
+#         self.name = name
+#         self.health = health
+#         self.level = level
+# player1 = Player("Player 1",10,0)
+# print(player1.name)
+# print(player1.health)
+# print(player1.level)
+#
+# class Character:
+#     __slots__ = ["name","health","level"]
+#     def __init__(self,name,health,level):
+#         self.name = name
+#         self.health = health
+#         self.level = level
+# player1 = Character("Player 1",10,0)
+# print(player1.name)
+# print(player1.health)
+# print(player1.level)
+# player1.health = 100
+# print(player1.health)
+# player1.weapon = "sword"
