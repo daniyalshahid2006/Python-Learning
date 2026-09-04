24fa0fd76d26a666e18ae78b215b1c6b4a4ea60f
Daniyal Shahid <daniyalshahid2006@gmail.com>
2026-09-04 19:22:25 +0500

    80 percent project done employee management and payroll

--- Json and csv project with advance oop/Employee Management & Payroll System.py
+++ Json and csv project with advance oop/Employee Management & Payroll System.py
@@ -1,6 +1,6 @@
 import csv
 
-
+headers = ["employee_id", "employee_name", "gmail", "department", "position", "salary"]
 class Employee:
     def __init__(self, employee_id, employee_name, gmail, department, position, salary):
         self.employee_id = employee_id
@@ -26,8 +26,8 @@ class EmployeeManager:
         candidate = 1
         while candidate in self.employees:
             candidate = candidate + 1
-            employee = Employee(candidate, employee_name, gmail, department, position, salary)
-            self.employees[candidate] = employee
+        employee = Employee(candidate, employee_name, gmail, department, position, salary)
+        self.employees[candidate] = employee
 
     def show_employees(self):
         for employee in self.employees.values():
@@ -56,18 +56,47 @@ class EmployeeManager:
             print("Employee removed")
         else:
             print("Employee not found")
-
+    def save_employee(self):
+        with open('employees.csv', "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(headers)
+            for employee in self.employees.values():
+                writer.writerow([employee.employee_id, employee.employee_name, employee.gmail,employee.department, employee.position, employee.salary])
+    def update_employee(self):
+        id = int(input("Enter employee id: "))
+        if id not in self.employees:
+            print("Employee not found")
+        else:
+            pick = 0
+            while pick != 6:
+                print("what you want to update?")
+                print("1.name")
+                print("2.email")
+                print("3.department")
+                print("4.position")
+                print("5.salary")
+                print("6.exit")
+                pick = int(input("Enter your choice: "))
+                if pick == 1:
+                    new_name = input("Enter new employee name: ")
+                    self.employees[id].employee_name = new_name
+                elif pick == 2:
+                    new_email = input("Enter new employee email: ")
+                    self.employees[id].gmail = new_email
+                elif pick == 3:
+                    new_department = input("Enter new employee department: ")
+                    self.employees[id].department= new_department
+                elif pick == 4:
+                    new_position = input("Enter new employee position: ")
+                    self.employees[id].position = new_position
+                elif pick == 5:
+                    new_salary = input("Enter new employee salary: ")
+                    self.employees[id].salary = new_salary
+                elif pick == 6:
+                    print("exit")
 
 employee_manager = EmployeeManager()
 
-
-with open('employees.csv', "w", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerow(["employee_id", "employee_name", "gmail", "department", "position", "salary"])
-    writer.writerows([
-        [1, "dani", "gmail", "ai", "employee", "100k"],
-        [2, "pani", "gmail", "ai", "employee", "100k"]
-    ])
 
 
 with open('employees.csv', "r") as file:
@@ -86,5 +115,6 @@ with open('employees.csv', "r") as file:
             employee_id, employee_name, gmail, department, position, salary
         )
 
-
 employee_manager.show_employees()
+employee_manager.remove_employee()
+employee_manager.save_employee()
