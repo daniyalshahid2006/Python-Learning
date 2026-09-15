1940344e502f0cfa593cbfddc45454e421474126
Daniyal Shahid <daniyalshahid2006@gmail.com>
2026-09-15 16:28:27 +0500

    module

---
+++ Python Modules & Packages/main.py
@@ -0,0 +1,7 @@
+# from calculator import add,divide,multiply,subtract
+# print(add(1,2))
+# print(divide(1,2))
+# print(multiply(1,2))
+# print(subtract(1,2))
+import student
+student.display_name()
---
+++ Python Modules & Packages/student.py
@@ -0,0 +1,3 @@
+def display_name():
+    print(input("Enter your first name: "))
+    print(input("Enter your last name: "))
