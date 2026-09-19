455eada61b2fa7cf314785cc2b85c00bb5323d03
Daniyal Shahid <daniyalshahid2006@gmail.com>
2026-09-19 19:59:06 +0500

    random

---
+++ Num_py/Random Numbers.py
@@ -0,0 +1,11 @@
+import numpy as np
+A = np.random.randint(1,20,size = 5)
+print(A)
+B = np.random.rand(5)
+print(B)
+C = np.random.rand(5,5)
+print(C)
+D = np.random.uniform(low=10,high=20,size = 5)
+print(D)
+np.random.seed(10)
+print(np.random.randint(1,20,size = 5))
